78640b9e941dddf844c8953d866dc38d4963e4ab
Nanda4312 <nandag1516@gmail.com>
2024-09-09 12:29:20 +0530

    conversations

---
+++ 02-conversations.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+
+echo "Ramesh:: Hi suresh, How are you?"
+echo "Suresh:: Hello ramesh, I am fine. How are you doing?"
+echo "Ramesh:: I am doing good Suresh. What's going on?"
+echo "Suresh:: I started learning Shell Script Ramesh"
