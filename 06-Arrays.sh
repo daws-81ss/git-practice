985c3f356b808008177d3c49dfe6255f503125f3
Nanda4312 <nandag1516@gmail.com>
2024-09-13 17:18:24 +0530

    Arrays concept

---
+++ 06-Arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("mango" "Apple" "kiwi")
+
+echo "first fruit is: ${FRUIT[0]}"
+echo "second fruit is: ${FRUIT[1]}"
+echo "third fruit is: ${FRUIT[2]}"
+
+
+echo "First fruit is: ${FRUITS[@]}"
