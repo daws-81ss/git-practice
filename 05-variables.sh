3c46a42217c7d1e2f3132810757b334a054a1a1a
Nanda4312 <nandag1516@gmail.com>
2024-09-13 16:56:44 +0530

    05-variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,6 +2,17 @@
 
 echo "please enter your username::"
 
-read USERNAME
+#read USERNAME  # here username is variable and takes input into it 
+
+read -s USERNAME # IF WE GIVE -S WE CANNOT SEE INPUT WHILE ENTERING DATA IN TERMINAL
 
 echo "username entered is: $USERNAME"
+
+echo "please enter password::"
+
+read -s PASSWORD
+
+echo "password entered is : $PASSWORD"
+
+
+
