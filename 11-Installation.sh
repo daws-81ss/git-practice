0346acd650d8c30620738853a11dbaef6e2f98f5
Nanda4312 <nandag1516@gmail.com>
2024-09-16 13:36:07 +0530

    Installations

--- 11-Installation.sh
+++ 11-Installation.sh
@@ -12,23 +12,39 @@ exit 1
 
 fi
 
-dnf list installed git  # Just checking whether installed or not
+# dnf list installed git  # Just checking whether installed or not
+
+# if [ $? -ne 0 ]
+
+# then 
+#     echo "Git going to installed, going to install it.."
+#     dnf install git -y    # here installing
+#     if [ $? -ne 0 ]
+#     then 
+#         echo "git installation is not success ..check it"
+#         exit 1
+#     else 
+#         echo "git installation is success"
+#     fi        
+# else
+#     echo "Git already installed, nothing to do.."
+
+# fi
+
+
+dnf list installed mysql  # checking whether mysql is installed or not
 
 if [ $? -ne 0 ]
-
 then 
-    echo "Git going to installed, going to install it.."
-    dnf install git -y    # here installing
+    echo "mysql is going to be install it"
+    dnf install mysql -y    # here installing
     if [ $? -ne 0 ]
-    then 
-        echo "git installation is not success ..check it"
+    then
+        echo "mysql installtion is not success"
         exit 1
-    else 
-        echo "git installation is success"
-    fi        
+    else
+        echo "mysql installation is successfully "   
 else
-    echo "Git already installed, nothing to do.."
-
+    echo "mysql is already installed,nothing to do"
 fi
-
 
