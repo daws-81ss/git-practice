0a0dbe13eefc18efac87de331959e67f1d04ddba
Nanda4312 <nandag1516@gmail.com>
2025-02-19 23:57:51 +0530

    shell-scripting

---
+++ 16-loops.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+}
+
+VALIDATE(){
+if [$1 -ne 0]
+
+
+}
